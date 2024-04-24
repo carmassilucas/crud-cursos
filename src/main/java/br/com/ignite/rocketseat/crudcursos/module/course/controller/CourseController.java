2abcfab600c4219de024c2ed913fc0445cad6bc0
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-24 18:29:14 -0300

    criando endpoint de deleção de curso

--- src/main/java/br/com/ignite/rocketseat/crudcursos/module/course/controller/CourseController.java
+++ src/main/java/br/com/ignite/rocketseat/crudcursos/module/course/controller/CourseController.java
@@ -4,6 +4,7 @@ import br.com.ignite.rocketseat.crudcursos.module.course.dto.CreateCourseRequest
 import br.com.ignite.rocketseat.crudcursos.module.course.dto.ListCourseRequestDto;
 import br.com.ignite.rocketseat.crudcursos.module.course.dto.UpdateCourseRequestDto;
 import br.com.ignite.rocketseat.crudcursos.module.course.useCase.CreateCourseUseCase;
+import br.com.ignite.rocketseat.crudcursos.module.course.useCase.DeleteCourseUseCase;
 import br.com.ignite.rocketseat.crudcursos.module.course.useCase.ListCourseUseCase;
 import br.com.ignite.rocketseat.crudcursos.module.course.useCase.UpdateCourseUseCase;
 import jakarta.validation.Valid;
@@ -25,6 +26,9 @@ public class CourseController {
     @Autowired
     private UpdateCourseUseCase updateCourseUseCase;
 
+    @Autowired
+    private DeleteCourseUseCase deleteCourseUseCase;
+
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody CreateCourseRequestDto dto) {
         var course = this.createCourseUseCase.execute(dto);
@@ -42,6 +46,16 @@ public class CourseController {
         try {
             var course = this.updateCourseUseCase.execute(id, dto);
             return ResponseEntity.ok().body(course);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable UUID id) {
+        try {
+            this.deleteCourseUseCase.execute(id);
+            return ResponseEntity.ok().build();
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
